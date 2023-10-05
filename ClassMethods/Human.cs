0ea0bf392be323d71ec58b3fdf451319956e6b2a
Marius Paškevičius <marius.paskevicius@yahoo.com>
2023-10-05 20:00:18 +0300

    Add project files.

---
+++ ClassMethods/Human.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ClassMethods
+{
+    internal class Human // <- klase
+    {
+        public string FirstName { get; set; }   // <- propertis (string tipo)
+        public string LastName { get; set; }    // <- propertis
+        public int Age { get; set; }            // <- propertis
+        public string Email { get; set; }      // <- propertis
+        public List<Pet> Pets { get; set; } = new List<Pet>();    // <- propertis (automatiskai inicializuojamas)
+
+        public Human(string firstName, string lastName, int age)    // <- konstruktorius
+        {
+            // priskiriame parametrus properciams:
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string GetFullName()             // <- klases metodas
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+        public void UpdateEmail(string newEmail)
+        {
+            if (IsValidEmail(newEmail))
+            {
+                Email = newEmail;
+            }
+            else
+            {
+                Console.WriteLine("Ivestas nevalidus email'as");
+            }
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            var regexPattern = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+            return Regex.IsMatch(email, regexPattern);
+        }
+
+        public void PrintPets()
+        {
+            foreach (var pet in Pets)
+            {
+                Console.WriteLine($"{pet.AnimalType}, {pet.Name}, {pet.Age}");
+            }
+        }
+    }
+}
---
+++ ClassMethods/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ClassMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Human("Frank", "Stevenson", 35);
+            var friend = new Human("Andrius", "Andraitis", 23);
+            var lecturer = new Human("Vilmantas", "Naviera", 25);
+
+            Console.WriteLine(employee.GetFullName());
+            Console.WriteLine(friend.GetFullName());
+            Console.WriteLine(lecturer.GetFullName());
+
+            employee.UpdateEmail("newFrankgmail.com");
+            friend.UpdateEmail("newFriend@gmail.com");
+            lecturer.UpdateEmail("newVilmantas@gmail.com");
+
+            Console.WriteLine(employee.Email);
+            Console.WriteLine(friend.Email);
+            Console.WriteLine(lecturer.Email);
+
+
+            var person = new Human("Albertas", "Majauskas", 35);
+            person.Pets.Add(new Pet("Suo", "Reksas", 5, "woof"));
+            person.Pets.Add(new Pet("Papuga", "Zibe", 15, "chirp"));
+            person.Pets.Add(new Pet("Kate", "Rainis", 3, "meow"));
+            person.PrintPets();
+
+            foreach(var pet in person.Pets)
+            {
+                pet.MakeSound();
+            }
+        }
+    }
+}
